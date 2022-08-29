b3f68f9c68c5cd22e4d26ea8559e723f3ada7d62
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-30 00:55:48 +0300

    completed the lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,42 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  delete ("/reviews/:id") do  
+    #find the review passed in params
+    #delete that review
+    #send the deleted review back as json for the tReact app to consume and show confirmation that it was deleted
+    id = params[:id].to_i
+    review = Review.find(id)
+    review.destroy  #This is the delete call
+    review.to_json  #return the deleted review
+  end
+
+  post("/reviews") do
+    review = Review.create(  
+      #Remember that all these are parameters 
+      id:params[:id],
+    score:params[:score],
+    comment:params[:comment],
+    game_id:params[:game_id],
+    user_id:params[:user_id]
+    )
+    review.to_json
+  end
+
+
+  patch("/reviews/:id") do
+    #find the review to update
+    #Access the data in the request body
+    #Use the data to update the review in the db
+    #return the updated review back to the frontend client
+    id = params[:id].to_i
+    review = Review.find(id)
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+
+    review.to_json
+  end
+
 end
--- config.ru
+++ config.ru
@@ -1,6 +1,6 @@
 require_relative './config/environment'
 
 # Parse JSON from the request body into the params hash
-use Rack::JSONBodyParser
+use Rack::JSONBodyParser  #this is a middleware. It runs before any other code runs when a post is done or a patch
 
 run ApplicationController
